3faf973f3f561197d7dc49e77a71f6cff8fdf336
YoelGarciaLago <145323567+YoelGarciaLago@users.noreply.github.com>
2023-11-17 09:52:52 +0100

    Delete docs directory

--- docs/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Funciones"},{"p":"<Unnamed>","l":"Main"},{"p":"<Unnamed>","l":"Trabajador"}];updateSearchResults();
